d37ff93824e46cc6de65c0511bd3dbcf374e325a
djh <1786008022@qq.com>
2020-09-16 10:08:45 +0800

    点赞

--- js/tryout.js
+++ js/tryout.js
@@ -3,3 +3,20 @@ $('.two_1').on('click',function(){
     $(this).find('span').text('请等待')
  })
  $('.one').find('a[href=#]').parent().css('border-bottom' ,'6px solid red');
+
+
+ function zan(){
+    $('.red').on('click',function(){
+        if($(this).hasClass('re') !=true){
+           $(this).attr('src','../../image/小图片/dianzan.png');
+           var three = $(this).next('span').text()-0;
+           $(this).next('span').text(three+1);
+           $(this).addClass('re');
+        }else{
+           $(this).attr('src','../../image/小图片/zan.png');
+           var three = $(this).next('span').text()-0;
+           $(this).next('span').text(three-1);
+           $(this).removeClass('re');
+        }
+    })
+}
